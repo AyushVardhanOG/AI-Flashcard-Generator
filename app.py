f1cc7fd46fe59e16b314983beb8af92e603024a9
AyushVardhanOG <ayushvardhan.main.email@gmail.com>
2026-06-01 11:20:25 +0530

    Created homepage UI

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello Ayush!")
-print("My AI Flashcard Generator Project Starts Today")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
